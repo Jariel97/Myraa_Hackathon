854a18f7f88b814af871c2eebfaa96f8ffe239df
Jariel Joash Gojar <jariel.gojar@gmail.com>
2018-02-03 18:55:16 +0530

    Introducing irrationality by making denominator not a power of 10

--- initial_code.py
+++ initial_code.py
@@ -1,9 +1,11 @@
 import numpy as n
-n_trials=10**7
+from decimal import Decimal, getcontext
+n_trials=10**7-3
 n_hits=0
-points = n.random.uniform(-1.0,1.0,(10**7,2))
+getcontext().prec=100
+points = n.random.uniform(Decimal(-1.0),Decimal(1.0),(n_trials,2))
 for a in points:
     if a[0]**2 + a[1]**2 <1 :
        n_hits+=1
-approx_pi=4*n_hits/n_trials
+approx_pi=4*Decimal(n_hits/n_trials)
 print(approx_pi)
